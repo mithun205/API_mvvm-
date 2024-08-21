45cea6093e0c1c1ad5c349723afaa93d9640703c
Mithun <126346172+mithun205@users.noreply.github.com>
2024-08-21 17:46:23 +0530

    api_service.dart

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -2,17 +2,21 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:mvvm_assignment/User_model.dart';
 
-
 class ApiService {
   final String apiUrl = "https://jsonplaceholder.typicode.com/users";
-  Future<List<User>> fetchUsers() async {
-    final response = await http.get(Uri.parse(apiUrl));
 
-    if (response.statusCode == 200) {
-      List<dynamic> data =json.decode(response.body);
-      return data.map((json) => User.fromJson(json)).toList();
-    } else {
-      throw Exception("Failed to load users");
+  Future<List<User>> fetchUsers() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        List<dynamic> data = json.decode(response.body);
+        return data.map((json) => User.fromJson(json)).toList();
+      } else {
+        throw Exception("Failed to load users");
+      }
+    } catch (e) {
+      throw Exception("Failed to connect to the server. Please check your internet connection.");
     }
   }
 }
